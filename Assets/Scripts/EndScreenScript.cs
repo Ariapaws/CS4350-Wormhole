064e7debdd48b11c224e1a1f1dce02ceebb75c44
Goh Hui May <lumiere_hm@hotmail.com>
2015-09-04 17:34:17 +0800

    update gitignore, add unity files

---
+++ Assets/Scripts/EndScreenScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndScreenScript : MonoBehaviour {
+
+	public int score = 0;
+	public Text scoreText;
+
+	private void Start(){
+		Cursor.visible = true;
+		GameObject notDes = GameObject.FindGameObjectWithTag ("NotDestroyed");
+		Score holder = notDes.GetComponent<Score>();
+		score = holder.score;
+		GameObject points = GameObject.FindGameObjectWithTag ("ScoreText");
+		Text scoreText = points.GetComponent<Text>();
+		scoreText.text = "Congratulations on completing WORMHOLE with " + score.ToString()+ " Points";
+
+	}
+	
+	public void OnClickPlay(){
+		Application.LoadLevel("MainGame");
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public Maze mazePrefab;
+	private Maze mazeInstance;
+	private bool pauseEnabled = false; 
+	GameObject player;
+
+	private IEnumerator Start () {
+		//BeginGame();
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+		yield return StartCoroutine(mazeInstance.Generate());
+
+	}
+	
+	private void Update () {
+		/*
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			//check if game is already paused       
+			if(pauseEnabled == true){
+				//unpause the game
+				Time.timeScale = 1;
+				pauseEnabled = false;
+			}
+			
+			//else if game isn't paused, then pause it
+			else if(pauseEnabled == false){
+				Time.timeScale = 0;
+				pauseEnabled = true;
+			}
+		}
+		*/
+
+
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		Debug.Log("OUT");
+		if(other.gameObject == player){
+			other.gameObject.GetComponent<PlayerHealth>().TakeDamage(300);
+		}
+	}
+	private void BeginGame () {
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+		StartCoroutine(mazeInstance.Generate());
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	private void RestartGame () {
+		StopAllCoroutines();
+		Destroy(mazeInstance.gameObject);
+		BeginGame();
+	}
+}
