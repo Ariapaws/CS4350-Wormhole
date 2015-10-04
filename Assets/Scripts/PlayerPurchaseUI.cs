f266ecc0f36bc0286349142f3bdc95e1ab4ab7cc
Rio <rs_neo@hotmail.com>
2015-10-05 01:10:08 +0800

    Added scripts for Shop and Purchase UI (not complete)

---
+++ Assets/Scripts/PlayerPurchaseUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerPurchaseUI : MonoBehaviour {
+
+	public bool showPurchaseUI;
+	public bool toggle;
+
+	public Text feedback;
+	public GameObject HUDcanvas = GameObject.FindGameObjectWithTag("Shop");
+	
+	// Use this for initialization
+	void Start () {
+		showPurchaseUI = false;
+		toggle = false;
+
+		feedback = GetComponent<Text>();
+		//playerInventory = gameObject.Find("Player").GetComponent(Inventory_GUI)
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.P)) {
+			if(toggle==false) {
+				Debug.Log ("P pressed!");
+				Debug.Log ("toggled 1");
+				showPurchaseUI = true;
+				PurchaseUI ();
+			}
+			if(toggle==true) {
+				Debug.Log ("toggled 0");
+				showPurchaseUI = false;
+				//hide purchase UI
+			}
+			toggle = !toggle;
+		}
+	}
+
+	void PurchaseUI(){
+		
+		
+
+	}
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Shop : MonoBehaviour {
+	
+	public bool canPurchase;
+	public float distance;
+	public GameObject player;
+
+	//public List<ShopItem> shopItems = new List<Shop>();
+
+	public Text feedback; 
+
+	// Use this for initialization
+	void Start () {
+		distance = 2.8f;
+		canPurchase = false;
+
+		InstantiateShop ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (this.gameObject.transform.position, player.transform.position) < distance) {
+			Debug.Log("Player is near.");
+			if (player.GetComponent<PlayerPurchaseUI>().showPurchaseUI == true) {
+				Debug.Log("Player can make purchase.");
+				Debug.Log(player.GetComponent<PlayerPurchaseUI>().showPurchaseUI);
+				canPurchase = true;
+			}
+		} else 
+			canPurchase = false;
+	}
+
+	void InstantiateShop () {
+		//fill shop array with items
+		// type, weapon name, attack damage, durability, cost
+		// type, invisible items name, upgrade number, durability, cost, upgrade name
+
+		// clear list
+		//List<Shop> shopItems = new List<Shop>();
+		/* shopItems.Clear();
+
+		//items type 1 (WEAPONS)
+		shopItems.Add (new Shop (1, "Dagger", 20, 500, 20));
+		shopItems.Add (new Shop (1, "Sword", 40, 100, 60));
+
+		//items type 2 (invisible items)
+		shopItems.Add (new Shop (2, "Helmet", 100, 100, 100, "health"));
+		shopItems.Add (new Shop (2, "Boots of Speed", 2, 100, 50, "speed"));
+		shopItems.Add (new Shop (2, "Armor", 100, 100, 200, "health"));
+		shopItems.Add (new Shop (2, "Ring", 1.1, 50, 200, "weapon_power"));
+
+		shopItems.Add (new Shop (2, "Potion", 50, 1, 20, "health")); */
+	}
+
+
+
+}
