2d10c407bf4e89b575e9771692d5da58437cf6d0
Goh Hui May <lumiere_hm@hotmail.com>
2015-10-05 00:46:17 +0800

    Added tag for torch prefab Updated MainGame scene Changed anchor settings for StartScreen scene Added LoadScreen scene and script - Loading not smooth yet, so have not connected it to StartScreen

---
+++ Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadScreen : MonoBehaviour {	
+	//public GameObject background;
+	public GameObject progressBar;
+	public GameObject textObject;
+	
+	private int loadProgress = 0;
+	
+	// Use this for initialization
+	void Start () {
+		//background.SetActive (false);
+		progressBar.SetActive (false);
+		textObject.SetActive (false);
+	}
+	
+	void Update(){
+		StartCoroutine(DisplayLoadingScreen("MainGame"));
+	}
+	
+	IEnumerator DisplayLoadingScreen(string level){
+		//background.SetActive (true);
+		progressBar.SetActive (true);
+		textObject.SetActive (true);
+		
+		progressBar.transform.localScale = new Vector3 (loadProgress, progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+
+		textObject.GetComponent<Text>().text = "Loading " + loadProgress + "%";
+		
+		AsyncOperation async = Application.LoadLevelAsync(level);
+		// async.allowSceneActivation = false;
+		while(!async.isDone){
+			loadProgress = (int)(async.progress * 100);
+			textObject.GetComponent<Text>().text = "Loading " + loadProgress + "%";
+			progressBar.transform.localScale = new Vector3(async.progress, progressBar.transform.localScale.y, progressBar.transform.localScale.z);
+
+			yield return null;
+		}
+		// async.allowSceneActivation = true;
+	}
+
+}
