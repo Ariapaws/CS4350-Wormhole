fd5daf5ba7611b7be88540f6fabd058557b58271
Goh Hui May <lumiere_hm@hotmail.com>
2015-10-19 00:57:01 +0800

    Update zombie attack script

--- Assets/Scripts/ZombieAttack.cs
+++ Assets/Scripts/ZombieAttack.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 public class ZombieAttack : MonoBehaviour {
 
 	public float timeBetweenAttacks = 0.5f;
+    public float ratioOftimeBeforeDamage = 0.4f;
+    public float ratioOfTimeForDamage = 0.2f;
+    public float durationOfAnimation = 2.0f;
 	public int attackDamage = 10;
 	public AudioClip attackClip;
 	
@@ -14,14 +17,20 @@ public class ZombieAttack : MonoBehaviour {
 	AudioSource zombieAudio;
 	ZombieHealth zombieHealth;
 	public bool playerInRange;
-	public float timer;
+	public float cooldownTimer;
+    private bool isCooldownTimerOn = true;
+    public float damageTimer;
+    private bool isDamageTimerOn = false;
+    private bool hasAttacked = false;
 	public bool isAttacking;
 
 	public Text feedback;
 	
 	void Awake ()
 	{
-		player = GameObject.FindGameObjectWithTag ("Player");
+        // Note if need to find which player to get aggro
+        // http://answers.unity3d.com/questions/598323/how-to-find-closest-object-with-tag-one-object.html
+        player = GameObject.FindGameObjectWithTag ("Player");
 		playerHealth = player.GetComponent <PlayerHealth> ();
 		zombieAudio = GetComponent <AudioSource> ();
 		zombieHealth = GetComponent<ZombieHealth>();
@@ -36,7 +45,6 @@ public class ZombieAttack : MonoBehaviour {
 		if(other.gameObject == player)
 		{
 			playerInRange = true;
-
 		}
 	}
 
@@ -44,7 +52,6 @@ public class ZombieAttack : MonoBehaviour {
 		if(other.gameObject == player)
 		{
 			playerInRange = true;
-			
 		}
 	}
 
@@ -53,78 +60,94 @@ public class ZombieAttack : MonoBehaviour {
 		if(other.gameObject == player)
 		{
 			playerInRange = false;
-			
 		}
 	}
 	
 	
 	void Update ()
 	{
-		timer += Time.deltaTime;
-		if(timer >= timeBetweenAttacks && playerInRange && zombieHealth.currentHealth > 0)
+        if (isCooldownTimerOn)
+        {
+            cooldownTimer += Time.deltaTime;
+        }
+        if (isDamageTimerOn)
+        {
+            damageTimer += Time.deltaTime;
+        }
+        if (isDamageTimerOn && damageTimer < durationOfAnimation * (ratioOftimeBeforeDamage + ratioOfTimeForDamage) && 
+            damageTimer >= durationOfAnimation * ratioOftimeBeforeDamage && 
+            playerInRange && zombieHealth.currentHealth > 0)
+        {
+            if (!hasAttacked)
+            {
+                hasAttacked = true;
+                Attack();   
+            }
+        }
+        if (isDamageTimerOn && damageTimer >= durationOfAnimation)
+        {
+            cooldownTimer = 0.0f;
+            isCooldownTimerOn = true;
+            damageTimer = 0.0f;
+            isDamageTimerOn = false;
+            hasAttacked = false;
+
+        }
+        if (isCooldownTimerOn && cooldownTimer >= timeBetweenAttacks && playerInRange && zombieHealth.currentHealth > 0)
 		{
 			if(anim){
 				anim.SetTrigger ("Attack");
-			} else {
-				GetComponent<Animation>().Play ("punch",PlayMode.StopSameLayer);
+                isDamageTimerOn = true;
+                isCooldownTimerOn = false;
 			}
-			Attack ();
-		}
-		/*
-		
-		if(playerHealth.currentHealth <= 0)
-		{
-			anim.SetTrigger ("PlayerDead");
-		}*/
-	}
-
-	bool attackComplete(){
-
-		Debug.Log ((anim.GetCurrentAnimatorStateInfo(0).IsName("GetHit")));
-	
-		if (this.isAttacking && !(anim.GetCurrentAnimatorStateInfo(0).IsName("GetHit")))
-		{
-			this.isAttacking = false;
-			Debug.Log("attack completed");
-			return true;
-			// You have just leaved your state!
-		} else if(anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
-		{
-			// Avoid any reload.
-			this.isAttacking = true;
-			Debug.Log("attack in progress");
-			return false;
-			
-		} else {
-			this.isAttacking = false;
-			Debug.Log("attack interrputed");
-			return false;
+            /*else {
+				GetComponent<Animation>().Play ("punch",PlayMode.StopSameLayer);
+			}*/
 		}
 	}
-	
-	
+
 	void Attack ()
 	{
-
-
-		if(playerHealth.currentHealth > 0 && attackComplete())
+		if(playerHealth.currentHealth > 0)
 		{
-			timer = 0f;
 			zombieAudio.clip = attackClip;
 			zombieAudio.Play ();
 			Vector3 direction = (new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z) - transform.position).normalized;
 			Vector3 newDir = Vector3.RotateTowards(transform.forward, direction, 2f, 0.0F);
 			transform.rotation = Quaternion.LookRotation(newDir);
-			if (playerHealth.armor >= attackDamage) {
-				feedback.color = new Color(102,178,255,1);
-				feedback.text = "Damage blocked";
-			}
-			else {
-				feedback.color = new Color(1,0,0,2);
-				feedback.text = "Damage received: "+attackDamage.ToString();
-				playerHealth.TakeDamage (attackDamage);
-			}
+			feedback.color = new Color(1,0,0,2);
+			feedback.text = "Damage received: "+attackDamage.ToString();
+			playerHealth.TakeDamage (attackDamage);
+
 		}
 
 	}
+
+    /********************
+       OLD CODE BELOW
+    ********************/
+    bool attackComplete()
+    {
+        if (this.isAttacking && !(anim.GetCurrentAnimatorStateInfo(0).IsName("GetHit")))
+        {
+            this.isAttacking = false;
+            Debug.Log("attack completed");
+            return true;
+
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            // Avoid any reload.
+            this.isAttacking = true;
+            Debug.Log("attack in progress");
+            return false;
+
+        }
+        else
+        {
+            this.isAttacking = false;
+            Debug.Log("attack interrputed");
+            return false;
+        }
+    }
 }
