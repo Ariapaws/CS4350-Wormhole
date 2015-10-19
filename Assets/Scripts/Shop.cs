46e3f29026fd9b04e1d34dfb78e1c554b82a5fa9
Rio <rs_neo@hotmail.com>
2015-10-19 21:49:54 +0800

    Fixed shop script (takes in values from list now).

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -8,11 +8,17 @@ public class Shop : MonoBehaviour {
 	
 	public bool canPurchase;
 	public float distance;
-
-	//public List<ShopItem> shopItems = new List<Shop>();
-
-	public Text feedback;
+    public Text feedback;
     public GameObject player;
+
+
+    public List<Upgrades> WeaponDamage;
+    public List<Upgrades> WeaponRange;
+    public List<Upgrades> WeaponSpeed;
+    public List<Upgrades> Armour;
+
+    public int currentWeaponDamageLevel;
+
 
 	// Use this for initialization
 	void Start () {
@@ -23,18 +29,17 @@ public class Shop : MonoBehaviour {
         GameObject feedbackObject = GameObject.FindGameObjectWithTag("Feedback");
         feedback = feedbackObject.GetComponent<Text>();
 
+        currentWeaponDamageLevel = 0;
+
 		InstantiateShop ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Vector3.Distance (this.gameObject.transform.position, player.transform.position) < distance) {
-			Debug.Log("Player is near.");
             feedback.color = new Color(0, 1, 0, 2);
             feedback.text = "You are near the shop. Press P to purchase.";
 			if (player.GetComponent<PlayerPurchaseUI>().showPurchaseUI == true) {
-				Debug.Log("Player can make purchase.");
-				Debug.Log(player.GetComponent<PlayerPurchaseUI>().showPurchaseUI);
 				canPurchase = true;
 			}
             else if (player.GetComponent<PlayerPurchaseUI>().showPurchaseUI == false) {
@@ -46,32 +51,41 @@ public class Shop : MonoBehaviour {
 
         if (canPurchase == true) { 
             // Press 1 (upgrade WEAPON ATTACK) --------------------------------------
-            //player.GetComponent<PlayerAssets>().currentCash
             if (Input.GetKeyUp(KeyCode.Alpha1)) {
-                // 1st upgrade = +5
-                if (CheckCost() == true) { player.GetComponent<PlayerAttack>().damagePerHit = 25; }
+                for (int i = 0; i < WeaponDamage.Count; i++) {
+                    Debug.Log("Checking if can upgrade..");
+                    if (WeaponDamage[i].level > currentWeaponDamageLevel) {
+                        Debug.Log("Can upgrade.");
+                        Debug.Log(WeaponDamage[i].cost);
+                        if (CheckCost(WeaponDamage[i].cost) == true) {
+                            Debug.Log("Enough gold.");
+                            player.GetComponent<PlayerAttack>().damagePerHit += WeaponDamage[i].upgradeAmount;
+                            currentWeaponDamageLevel = WeaponDamage[i].level;
+                            break;
+                        }
+                    }
+                }
             }
 
             // Press 2 (upgrade WEAPON RANGE) --------------------------------------
             if (Input.GetKeyUp(KeyCode.Alpha2))
             {
-                // 1st upgrade = +0.5
-                if (CheckCost() == true) { player.GetComponent<PlayerAttack>().range = 5.5f; }
+                //if (CheckCost() == true) { player.GetComponent<PlayerAttack>().range = 5.5f; }
             }
 
-            // Press 3 (upgrade ARMOUR--health) --------------------------------------
+            // Press 3 (upgrade ATTACK SPEED) --------------------------------------
             if (Input.GetKeyUp(KeyCode.Alpha3))
             {
-                // 1st upgrade = +50 health
-                if (CheckCost() == true) { player.GetComponent<PlayerHealth>().startingHealth = 350; }
+                //if (CheckCost() == true) { player.GetComponent<PlayerAttack>().attackSpeed = 1.2f; }
             }
 
-            // Press 4 (upgrade SPEED) --------------------------------------
+            // Press 4 (upgrade ARMOUR--health) --------------------------------------
             if (Input.GetKeyUp(KeyCode.Alpha4))
             {
-                // 1st upgrade = +50 health
-                //player.GetComponent<FirstPersonController>().startingHealth = 350;
+                //if (CheckCost() == true) { player.GetComponent<PlayerHealth>().startingHealth = 350; }
             }
+
+
 
             // Press 5 (purchase POTION) --------------------------------------
 
@@ -81,28 +95,34 @@ public class Shop : MonoBehaviour {
 
 	void InstantiateShop () {
 		//fill shop array with items
-		// type, weapon name, attack damage, durability, cost
-		// type, invisible items name, upgrade number, durability, cost, upgrade name
+		// button type, upgrade name, weapon level--still upgradable?, cost, upgrade amount 
 
-		// clear list
-		//List<Shop> shopItems = new List<Shop>();
-		/* shopItems.Clear();
 
-		//items type 1 (WEAPONS)
-		shopItems.Add (new Shop (1, "Dagger", 20, 500, 20));
-		shopItems.Add (new Shop (1, "Sword", 40, 100, 60));
+		// STEP 1: clear list
+	    WeaponDamage = new List<Upgrades>();
+		WeaponDamage.Clear();
 
-		//items type 2 (invisible items)
-		shopItems.Add (new Shop (2, "Helmet", 100, 100, 100, "health"));
-		shopItems.Add (new Shop (2, "Boots of Speed", 2, 100, 50, "speed"));
-		shopItems.Add (new Shop (2, "Armor", 100, 100, 200, "health"));
-		shopItems.Add (new Shop (2, "Ring", 1.1, 50, 200, "weapon_power"));
+        
+        // STEP 2: fill list with items info
+		//items type 1 (upgrades)
+        //ALPHA 1
+        WeaponDamage.Add(new Upgrades(1, "Weapon Damage", 1, 0, 5));
+        WeaponDamage.Add(new Upgrades(1, "Weapon Damage", 2, 0, 5));
+        //ALPHA 2
+		//shopItems.Add (new Shop (2, "Weapon Range", 1, 100, 0.5f));
+        //ALPHA 3
+		//shopItems.Add (new Shop (3, "Armor", 1, 100, 50));
+        //ALPHA 4
+		//shopItems.Add (new Shop (4, "Boots", 1, 100, 0.5f));
+               
+        //items type 2 (consumables)
+        //ALPHA 5
+		//shopItems.Add (new Shop (5, "Potion", 1, 30, 50));
 
-		shopItems.Add (new Shop (2, "Potion", 50, 1, 20, "health")); */
 	}
 
-    bool CheckCost() {
-        if (player.GetComponent<PlayerAssets>().currentCash >= 0)
+    bool CheckCost(int upgradeCost) {
+        if (upgradeCost <= player.GetComponent<PlayerAssets>().currentCash)
         {
             return true;
         }
---
+++ Assets/Scripts/Upgrades.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System; //This allows the IComparable Interface
+
+//This is the class you will be storing
+//in the different collections. In order to use
+//a collection's Sort() method, this class needs to
+//implement the IComparable interface.
+public class Upgrades : IComparable<Upgrades>
+{
+    public int type;
+    public string name;
+    public int level;
+    public int cost;
+    public int upgradeAmount;
+
+    public Upgrades(int newType, string newName, int newLevel, int newCost, int newUpgradeAmount)
+    {
+        // button type, upgrade name, weapon level--still upgradable?, cost, upgrade amount
+        type = newType;
+        name = newName;
+        level = newLevel;
+        cost = newCost;
+        upgradeAmount = newUpgradeAmount;
+    }
+
+    //This method is required by the IComparable
+    //interface. 
+    public int CompareTo(Upgrades other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+        else
+            return 0;
+    }
+}
